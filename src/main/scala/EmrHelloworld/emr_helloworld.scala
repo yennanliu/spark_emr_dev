7e69af52033b4a9424606a21cfe7efebdecc1a62
yennanliu <f339339@gmail.com>
2019-11-24 18:37:41 +0800

    add emr helloworld script

---
+++ src/main/scala/EmrHelloworld/emr_helloworld.scala
@@ -0,0 +1,18 @@
+package EmrHelloworld
+
+import org.apache.spark._
+
+object emr_helloworld {
+  def main(args: Array[String]): Unit = {
+    //setup spark
+    val sc = new SparkContext(new SparkConf())
+    //your algorithm
+    val n = 10000000
+    val count = sc.parallelize(1 to n).map { i =>
+      val x = scala.math.random
+      val y = scala.math.random
+      if (x * x + y * y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("Pi is roughly " + 4.0 * count / n)
+  }
+}
