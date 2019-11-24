cd36d22d8f9e9345fc341d28785b6fdd27d7a7bc
yennanliu <f339339@gmail.com>
2019-11-25 00:17:44 +0800

    fix spark sbt compile / syntax  error

--- src/main/scala/EmrHelloworld/emr_helloworld.scala
+++ src/main/scala/EmrHelloworld/emr_helloworld.scala
@@ -1,11 +1,14 @@
 package EmrHelloworld
 
 import org.apache.spark._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object emr_helloworld {
   def main(args: Array[String]): Unit = {
     //setup spark
-    //val sc = new SparkContext(new SparkConf())
+    val conf = new SparkConf().setAppName("Spark emr Hello world")
+
+    val sc = new SparkContext(conf)
     //your algorithm
     val n = 10000000
     val count = sc.parallelize(1 to n).map { i =>
--- src/main/scala/EmrHelloworld/spark_helloworld.scala
+++ src/main/scala/EmrHelloworld/spark_helloworld.scala
@@ -1,21 +1,28 @@
 package EmrHelloworld
 
 import org.apache.spark._
+// import required spark classes
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object spark_helloworld {
   def main(args: Array[String]): Unit = {
     //setup spark
-    //val sc = new SparkContext(new SparkConf())
+    val conf = new SparkConf().setAppName("SparkHelloWorld")
+    val spark: SparkSession = SparkSession.builder
+                                          .config(conf)
+                                          .getOrCreate()
+
     // do stuff
     println("************")
     println("************")
     println("Hello, world!")
-    val rdd = sc.sparkContext.parallelize(Array(1 to 10))
+    val rdd = spark.sparkContext.parallelize(Array(1 to 10))
     rdd.count()
     println("************")
     println("************")   
     // terminate spark context
-    spark.stop()
+    //spark.stop()
 
   }
 }
