c354da8f2d04b2c5d5969fc976aa268cca5e50ba
yennanliu <f339339@gmail.com>
2019-12-07 17:46:47 +0800

    add songs_table.sql

---
+++ athena/songs_table.sql
@@ -0,0 +1,13 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS sparkify_song.songs_table (
+  `song_id` string,
+  `title` string,
+  `artist_id` string,
+  `year` string,
+  `duration` DOUBLE
+
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1'
+) LOCATION 's3://sparkify-song-data/etl_output/songs_table/'
+TBLPROPERTIES ('has_encrypted_data'='false');
