6d76711fc64956f0b0e85f3f86ff1f48acc64584
yennanliu <f339339@gmail.com>
2019-12-07 19:33:03 +0800

    add process_log_data.scala

---
+++ src/main/scala/SparkifySongETL/process_log_data.scala
@@ -0,0 +1,40 @@
+package SparkifySongETL
+
+import org.apache.spark._
+// import required spark classes
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object process_log_data {
+  def main(args: Array[String]): Unit = {
+    //setup spark
+    val conf = new SparkConf().setMaster("local[*]")
+                              .setAppName("process_log_data")
+    val spark: SparkSession = SparkSession.builder
+                                          .config(conf)
+                                          .getOrCreate()
+
+    var input_data = "s3a://sparkify-song-data//log-data" 
+    var output_data = "s3a://sparkify-song-data/scala_etl_output/process_log_data" 
+    //var log_data = input_data +  "/*/*/*/*.json"
+    var log_data = input_data +  "/2018/11/*.json"
+
+    print (">>>>>> load from S3")
+    var df = spark.read.json(log_data)
+    df.createOrReplaceTempView("log_data_table")
+
+    var logs_table = spark.sql("""
+                            SELECT distinct log.* 
+                            FROM log_data_table log
+                            WHERE song IS NOT NULL
+                        """)
+
+    print (">>>>>> write to S3")
+    logs_table.write
+               .mode("overwrite")
+               .partitionBy("userid", "method")
+               .parquet(output_data)
+    spark.stop()
+
+  }
+}
