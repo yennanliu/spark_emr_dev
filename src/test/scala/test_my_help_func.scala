fb6a29a7fddb9f7219abad60366c7f6513c68a2c
yennanliu <f339339@gmail.com>
2019-12-08 20:09:49 +0800

    add scala test

---
+++ src/test/scala/test_my_help_func.scala
@@ -0,0 +1,24 @@
+import collection.mutable.Stack
+import org.scalatest._
+import org.scalatest.FlatSpec
+import org.scalatest.tagobjects.Slow
+
+//UDF 
+import myhelpclass.my_help_func
+
+class test_my_help_func extends FlatSpec {
+
+    "A add func" should "add input values x and y " in {
+    var x = 1 
+    var y = 2 
+    var result = my_help_func.my_sum_func(x, y)
+    assert ( result === 3 )
+    }
+
+    "A product func" should "product input values x and y " in {
+    var x = 1 
+    var y = 2 
+    var result = my_help_func.my_product_func(x, y)
+    assert ( result === 2 )
+    }
+}
