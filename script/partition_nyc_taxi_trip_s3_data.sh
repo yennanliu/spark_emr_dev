fdd47366b797d653635cf75947c57f79350cd8d8
yennanliu <f339339@gmail.com>
2019-12-18 17:51:31 +0800

    fix to_dump bucket name and uncomment  execute run code

--- script/partition_nyc_taxi_trip_s3_data.sh
+++ script/partition_nyc_taxi_trip_s3_data.sh
@@ -6,9 +6,9 @@
 partition_s3_file() { 
 
 from_bucket=s3://nyc-tlc-taxi/trip_data
-to_yellow_bucket=s3://nyc-tlc-taxi/trip_data/yellow_trip
-to_green_bucket=s3://nyc-tlc-taxi/trip_data/green_trip
-to_fhv_bucket=s3://nyc-tlc-taxi/trip_data/fhv_trip
+to_yellow_bucket=s3://nyc-tlc-taxi/yellow_trip
+to_green_bucket=s3://nyc-tlc-taxi/green_trip
+to_fhv_bucket=s3://nyc-tlc-taxi/fhv_trip
 s3_files=`aws s3 ls s3://nyc-tlc-taxi/trip_data/`
 
 for s3_file in $s3_files;
@@ -17,20 +17,20 @@ for s3_file in $s3_files;
        then   
          echo $s3_file
          to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
-         echo "copy" $from_bucket/$s3_file "to" $to_yellow_bucket/dt=$to_file/$s3_file 
-         #aws s3 cp $from_bucket/$s3_file $to_bucket/dt=$to_file/$s3_file
+         #echo "copy" $from_bucket/$s3_file "to" $to_yellow_bucket/dt=$to_file/$s3_file 
+         aws s3 cp $from_bucket/$s3_file $to_yellow_bucket/dt=$to_file/$s3_file
     
     elif [[ $s3_file =~ ^green_tripdata ]] 
         then  
           to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
-          echo "copy" $from_bucket/$s3_file "to" $to_green_bucket/dt=$to_file/$s3_file
-          #aws s3 cp $from_bucket/$s3_file $to_bucket/dt=$to_file/$s3_file 
+          #echo "copy" $from_bucket/$s3_file "to" $to_green_bucket/dt=$to_file/$s3_file
+          aws s3 cp $from_bucket/$s3_file $to_green_bucket/dt=$to_file/$s3_file 
 
     elif [[ $s3_file =~ ^fhv_tripdata ]] 
         then  
           to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
-          echo "copy" $from_bucket/$s3_file "to" $to_fhv_bucket/dt=$to_file/$s3_file 
-          #aws s3 cp $from_bucket/$s3_file $to_bucket/dt=$to_file/$s3_file
+          #echo "copy" $from_bucket/$s3_file "to" $to_fhv_bucket/dt=$to_file/$s3_file 
+          aws s3 cp $from_bucket/$s3_file $to_fhv_bucket/dt=$to_file/$s3_file
     else 
         continue 
     fi 
