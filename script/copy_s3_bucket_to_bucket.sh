9410d125abd9d5499894075f184e9f5f91b096e8
yennanliu <f339339@gmail.com>
2019-12-18 17:01:17 +0800

    add main logic copy s3 -> partitioned s3 bucket and split pure copy command to another script (copy_s3_bucket_to_bucket.sh)

---
+++ script/copy_s3_bucket_to_bucket.sh
@@ -0,0 +1,6 @@
+copy_s3_file_from_bucket_to_bucket() {
+
+aws s3  cp --recursive "s3://nyc-tlc-taxi/trip data/" "s3://nyc-tlc-taxi/trip_data/"
+}
+
+copy_s3_file_from_bucket_to_bucket
--- script/partition_nyc_taxi_trip_s3_data.sh
+++ script/partition_nyc_taxi_trip_s3_data.sh
@@ -3,32 +3,36 @@
 # HELP SCRIPT PARTITION S3 FILES 
 #################################################################
 
-copy_s3_file_from_bucket_to_bucket() {
-
-aws s3  cp --recursive "s3://nyc-tlc-taxi/trip data/" "s3://nyc-tlc-taxi/trip_data/"
-}
-
 partition_s3_file() { 
 
 from_bucket=s3://nyc-tlc-taxi/trip_data
-to_bucket=s3://nyc-tlc-taxi/trip_data/yellow_trip
-#s3_files=`aws s3 ls s3://nyc-tlc-taxi/trip_data/ | cut -d " " -f 4`
-#s3_files=`aws s3 ls s3://nyc-tlc-taxi/trip_data/ | cut -d " " -f 3`
+to_yellow_bucket=s3://nyc-tlc-taxi/trip_data/yellow_trip
+to_green_bucket=s3://nyc-tlc-taxi/trip_data/green_trip
+to_fhv_bucket=s3://nyc-tlc-taxi/trip_data/fhv_trip
 s3_files=`aws s3 ls s3://nyc-tlc-taxi/trip_data/`
-
 
 for s3_file in $s3_files;
     do
     if [[ $s3_file =~ ^yellow_tripdata ]] 
-       then   echo $s3_file
-       #to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 2 | cut -d "." -f 1)
-       #echo "copy" $from_bucket/$s3_file "to" $to_bucket/dt=$to_file/$s3_file 
-       #aws s3 cp $from_bucket/$s3_file $to_bucket/dt=$to_file/$s3_file
+       then   
+         echo $s3_file
+         to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
+         #echo $to_file
+         echo "copy" $from_bucket/$s3_file "to" $to_yellow_bucket/dt=$to_file/$s3_file 
+         #aws s3 cp $from_bucket/$s3_file $to_bucket/dt=$to_file/$s3_file
     elif [[ $s3_file =~ ^green_tripdata ]] 
-        then   echo $s3_file
+        then  
+          #continue
+          to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
+          echo "copy" $from_bucket/$s3_file "to" $to_green_bucket/dt=$to_file/$s3_file 
+          #echo $s3_file
 
     elif [[ $s3_file =~ ^fhv_tripdata ]] 
-        then   echo $s3_file
+        then  
+          #continue 
+          to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 3 | cut -d "." -f 1)
+          echo "copy" $from_bucket/$s3_file "to" $to_fhv_bucket/dt=$to_file/$s3_file 
+          #echo $s3_file
     else 
         continue 
     fi 
