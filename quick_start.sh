ae613948c44502a8548a3e6de2f2617afc526bf7
yennanliu <f339339@gmail.com>
2019-12-25 17:46:45 +0800

    add install sbt func (quick_start.sh)

--- quick_start.sh
+++ quick_start.sh
@@ -3,14 +3,50 @@ quick_start() {
     echo "### HELP SCRIPT RUN REPO COMMANDS ### "
     echo ""
 
-    echo "run_sbt_test      : " "bash quick_start.sh  run_sbt_test"
-    echo "run_sbt_compile   : " "bash quick_start.sh  run_sbt_compile"
-    echo "run_sbt_run       : " "bash quick_start.sh  run_sbt_run"
-    echo "run_sbt_test      : " "bash quick_start.sh  run_sbt_test"
-    echo "run_sbt_assembly  : " "bash quick_start.sh  run_sbt_assembly"
+    echo "install_sbt_at_emr : " "bash quick_start.sh  install_sbt_at_emr"
+    echo "run_sbt_test       : " "bash quick_start.sh  run_sbt_test"
+    echo "run_sbt_compile    : " "bash quick_start.sh  run_sbt_compile"
+    echo "run_sbt_run        : " "bash quick_start.sh  run_sbt_run"
+    echo "run_sbt_test       : " "bash quick_start.sh  run_sbt_test"
+    echo "run_sbt_assembly   : " "bash quick_start.sh  run_sbt_assembly"
 
     echo ""
     echo "### HELP SCRIPT RUN REPO COMMANDS ### "
+}
+
+install_sbt_at_emr() {
+
+    set +e
+    cd ~ 
+    export PATH=$PATH:/opt/sbt/bin
+    if [ -n `which sbt` ]; then 
+            echo "sbt alrady installed"
+            which sbt
+        else
+        cd ~
+        wget https://github.com/sbt/sbt/releases/download/v0.13.15/sbt-0.13.15.tgz
+        tar xf sbt-0.13.15.tgz
+        sudo mv sbt /opt
+        export PATH=$PATH:/opt/sbt/bin
+        echo "run sbt test..."
+        sbt test
+    fi
+    set -e
+}
+
+install_java(){
+  set +e
+  if [ -n `which java` ]; then 
+  echo 'java install OK'
+  which java
+  java -version  
+  else 
+  echo 'No java installed, please install it for running spark'
+  echo 'install java 8 OpenJDK via apt...'
+  apt install openjdk-8-jdk
+  fi 
+  set -e 
+
 }
 
 run_sbt_test() {
