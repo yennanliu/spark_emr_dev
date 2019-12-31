93ab30baf82718420bc90343f1fe61691c7a21bf
yennanliu <f339339@gmail.com>
2019-12-31 23:22:21 +0800

    add to run spark-submit on the exported scala jar (travis_build.sh)

--- travis_build.sh
+++ travis_build.sh
@@ -8,4 +8,4 @@ type=bind,\
 source="$(pwd)"/.,\
 target=/spark_emr_dev \
 -i -t spark_env \
-/bin/bash  -c "cd ../spark_emr_dev && sbt clean compile && sbt assembly"
+/bin/bash  -c "cd ../spark_emr_dev && sbt clean compile && sbt assembly && spark-submit /spark_emr_dev/target/scala-2.11/spark-etl-pipeline-assembly-1.0.jar"
